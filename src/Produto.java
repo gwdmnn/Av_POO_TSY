066e4b36fbe3b5960dd93dbabf032ee399fbceba
Guilherme <guilherme.weidmann@gmail.com>
2022-10-06 19:58:18 -0300

    Correção Nino

--- src/Produto.java
+++ src/Produto.java
@@ -27,8 +27,24 @@ public class Produto extends Marca{
     }
 
     public void displayProduto(){
-        System.out.println("Nome do Produto: " + this.nomeProduto);
+        System.out.println("Nome do produto: " + this.nomeProduto);
         System.out.println("Marca do produto: " + this.marcaProduto);
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (o == this){                             //compara se não é ele mesmo
+            return true;
+        }
+        if(o == null){                              //verifica se o produto comparado não é nulo
+            return false;
+        }
+        if (!(o instanceof Produto)){               //verifica se o produto de entrada é mesmo uma instância de produto
+            return false;
+        }
+        Produto outroProduto = (Produto)o;
+        boolean result = this.nomeProduto.equals(outroProduto.nomeProduto) && this.marcaProduto.equals(outroProduto.marcaProduto);
+        return result;
     }
 
 }
